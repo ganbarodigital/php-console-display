443f1d9555ba283df995946917a7b89af38556ea
Stuart Herbert <stuart@stuartherbert.com>
2015-07-07 07:45:19 +0100

    WIP: new StringWriter

---
+++ src/Internal/OutputWriters/StringWriter.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Copyright (c) 2010, Gradwell dot com Ltd
+ * Copyright (c) 2011, Stuart Herbert
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   ConsoleDisplay/Internal
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2010 Gradwell dot com Ltd www.gradwell.com
+ * @copyright 2011 Stuart Herbert www.stuartherbert.com
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-console-display
+ */
+
+namespace GanbaroDigital\ConsoleDisplay\Internal\OutputWriters;
+
+use GanbaroDigital\ConsoleDisplay\Exceptions\E4xx_UnsupportedType;
+
+class StringWriter implements OutputWriter
+{
+    /**
+     * the string that we are going to write our output to
+     * @var string
+     */
+    private $buffer;
+
+    public function __construct(&$buffer)
+    {
+        $this->setBuffer($buffer);
+    }
+
+    /**
+     * our string to append to
+     *
+     * @param string $buffer
+     * @return void
+     */
+    protected function setBuffer(&$buffer)
+    {
+        if (!is_string($buffer)) {
+            throw new E4xx_UnsupportedType(gettype($buffer));
+        }
+
+        $this->buffer = $buffer;
+    }
+
+    /**
+     * flush the current stream
+     *
+     * @return void
+     */
+    protected function flush()
+    {
+        // no-op
+    }
+
+    /**
+     * write a string to the output buffer
+     *
+     * @param  string $output
+     *         the string to write
+     * @return void
+     */
+    public function writeContent($output)
+    {
+        $this->buffer .= $output;
+    }
+
+    /**
+     * write a string of control code(s) to the output stream
+     *
+     * it is up to each stream to decide whether or not control codes
+     * affect the current line position
+     *
+     * @param  string $output
+     *         the control codes to write
+     * @return void
+     */
+    public function writeControlCodes($output)
+    {
+        $this->writeContent($output);
+    }
+
+    /**
+     * write a new-line character sequence to the output stream
+     *
+     * @return void
+     */
+    public function writeNewline()
+    {
+        $this->buffer .= PHP_EOL;
+    }
+}
